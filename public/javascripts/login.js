339a95b317c6e83b882b038f30601507674a9fda
Susan Xiao <susanxiao@nyu.edu>
2018-04-19 21:07:13 -0400

    deleted old implementation files

--- public/javascripts/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', evt => {
-  // TODO: turn login/register into popups
-  document.querySelector('.button#create-button').addEventListener('click', evt => {
-    window.location.href = '/login';
-  });
-  document.querySelector('.join-button').addEventListener('click', evt => {
-    window.location.href = '/login';
-  });
-});
